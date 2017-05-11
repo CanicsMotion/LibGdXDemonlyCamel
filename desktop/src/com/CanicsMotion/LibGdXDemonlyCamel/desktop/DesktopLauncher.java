bebd38a10f1e9ea4925f68d056b88de0f475c0e3
janekx21 <janekx21@gmail.com>
2017-05-11 22:39:49 +0200

    import lib gdx

---
+++ desktop/src/com/CanicsMotion/LibGdXDemonlyCamel/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.CanicsMotion.LibGdXDemonlyCamel.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.CanicsMotion.LibGdXDemonlyCamel.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
